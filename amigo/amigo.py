fb6ca89658e5da536dcc5008cea877a374930881
hecris <christopher.he33@myhunter.cuny.edu>
2018-10-08 22:51:28 -0400

    added comments to amigo.py
    Update README.md

--- amigo/amigo.py
+++ amigo/amigo.py
@@ -22,16 +22,23 @@ def conjugate(verb):
 
 def pretty_table(html):
 	space = ' '
+	# number of columns
 	n = len(html.find('tr'))
+	# get all td elements in table
 	data = html.find_all('td')
-	build = "| "
+	# this is the string to build upon. it represents one row
+	build = "|"
 
 	for i in range(len(data)):
 		td = data[i].text
-		diff = 20 - len(td)
+		# each cell in the table will be allowed 22 characters.
+		# the remaining will be filled with spaces.
+		# we use the vertical bar character to separate the cells
+		diff = 22 - len(td)
 		build = build + td + diff * space + '|'
 		colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+		# if row is complete, echo it
 		if (i + 1) % n == 0:
 			click.echo(click.style(build, fg=colors[i // n]))
-			build = "| "
+			build = "|"
 		
