4e4cd66eddcb35421e34de7bc094ad61f66ce9b1
Christopher He <christopher.he33@myhunter.cuny.edu>
2018-10-11 13:33:17 -0400

    Revert "added BoseCorp/py-googletrans to setup.py"

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup (
 	install_requires=[
 		"Click",
 		"Requests",
-		"BeautifulSoup4",
-		"git+https://github.com/BoseCorp/py-googletrans.git"
+		"BeautifulSoup4"
 		],
 	entry_points="""
 		[console_scripts]
